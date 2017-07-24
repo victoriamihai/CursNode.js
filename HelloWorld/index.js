65f874ae6fee3d3797441a5264914ac618d60743
Mihai, Victoria <victoria.mihai@sap.com>
2017-07-24 11:23:01 +0300

    Hello world with nodeJs server

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -3,3 +3,23 @@ console.log("hello world!");
 console.log("test git");
 var mesaj = "ceva";
 console.log(mesaj);
+var sMessage = "Mesaj RESponse";
+
+//inregistrare 2
+//http = modul
+//require module = include un modul, pachete
+var http = require("http");
+//variabile
+var iPort = 8081;
+//creaza http server
+var server = http.createServer(function(req, res){
+    console.log("Request recived");
+    //ca sa returnez un raspuns (res)
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.write(sMessage);
+    res.end();
+    console.log('response send');
+});
+//severul are un listen board, pe care il punem pe port
+server.listen(iPort);
+console.log("server running at http://127.0.0.1:" + iPort + "/");
