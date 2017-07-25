2acf1b3a21e77aef6907d555606eacefb1f2c670
Mihai, Victoria <victoria.mihai@sap.com>
2017-07-25 11:25:44 +0300

    update rest example

--- RestExample/index.js
+++ RestExample/index.js
@@ -7,7 +7,19 @@ app.get('/users', function (req, res) {
        console.log( data );
        res.end( data );
    });
-})
+});
+app.put('/users', function (req, res) {
+//handle create user
+});
+
+app.post('/users', function (req, res) {
+//handle update user
+});
+
+app.delete('/users', function (req, res) {
+//handle delete user
+}); 
+
 
 var server = app.listen(8081, function () {
 
@@ -16,4 +28,4 @@ var server = app.listen(8081, function () {
 
   console.log("Example app listening at http://%s:%s", host, port)
 
-})
+});
