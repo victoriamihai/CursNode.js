1ddd51f5ecbdd368c8a53a4cce8c2d643fa3f95f
Mihai, Victoria <victoria.mihai@sap.com>
2017-07-26 08:53:59 +0300

    password validator Unit Test

--- UnitTestExample/test/password_valiadator_test.js
+++ UnitTestExample/test/password_valiadator_test.js
@@ -1,7 +1,18 @@
+var chai = require("chai");
+var passwordValidator = require("../modules/password_validator.js")
+
 describe("passwordValidator", function() {//se refera la module
    describe(".validPassword(password)", function() {//se refera la functie
        it("trebuie sa aibe cel putin 8 caractere", function(){
            //Test Goes Here
+           var wrongPass = "1234567";
+           var valid = passwordValidator.validPassword(wrongPass);
+           //chai check
+           chai.expect(valid).to.be.equal(false);
+           //test with goot pass
+           var goodPass = "12345678";
+           var valid2 = passwordValidator.validPassword(goodPass);
+           chai.expect(valid2).to.be.equal(true);
        });
    });
 });
